0c82d063a19f3448b968640e5ef6312054cde6c4
Vlad Silviu Hagiu <vladsilviu.hagiu@endava.com>
2022-07-29 14:21:55 +0300

    Code review resolved

--- Internship2022TeamPatri/Internship2022TeamPatri/Controllers/Auth/Login/LogInViewController.swift
+++ Internship2022TeamPatri/Internship2022TeamPatri/Controllers/Auth/Login/LogInViewController.swift
@@ -10,18 +10,15 @@ import SnapKit
 
 class LogInViewController: UIViewController {
     
-    @IBOutlet weak var passwordLabel: UILabel!
-    @IBOutlet weak var emailLabel: UILabel!
-    @IBOutlet weak var logInLabel: UILabel!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var notRegisteredLabel: UILabel!
-    @IBOutlet weak var signupButton: UIButton!
-    @IBOutlet weak var forgotPasswordButton: UIButton!
-    
-    let colorText = UIColor(red: 37/255, green: 22/255, blue: 5/255, alpha: 1)
-    let colorView = UIColor(red: 124/255, green: 122/255, blue: 122/255, alpha: 1)
+    @IBOutlet weak private var passwordLabel: UILabel!
+    @IBOutlet weak private var emailLabel: UILabel!
+    @IBOutlet weak private var logInLabel: UILabel!
+    @IBOutlet weak private var emailTextField: UITextField!
+    @IBOutlet weak private var passwordTextField: UITextField!
+    @IBOutlet weak private var loginButton: UIButton!
+    @IBOutlet weak private var notRegisteredLabel: UILabel!
+    @IBOutlet weak private var signupButton: UIButton!
+    @IBOutlet weak private var forgotPasswordButton: UIButton!
     
     let yourAttributesSignUp: [NSAttributedString.Key: Any] = [
           .underlineStyle: NSUnderlineStyle.single.rawValue,
@@ -53,13 +50,11 @@ class LogInViewController: UIViewController {
         setUpElements()
     }
     
-    
-    
-    func setUpElements() {
-        logInLabel.textColor = colorText
-        signupButton.tintColor = colorText
-        forgotPasswordButton.tintColor = colorText
-        notRegisteredLabel.textColor = colorText
+    private func setUpElements() {
+        logInLabel.textColor = .colorText
+        signupButton.tintColor = .colorText
+        forgotPasswordButton.tintColor = .colorText
+        notRegisteredLabel.textColor = .colorText
         
         logInLabel.font = UIFont.boldSystemFont(ofSize: 35)
         signupButton.titleLabel?.font = UIFont.systemFont(ofSize: 22, weight: .bold)
@@ -77,65 +72,60 @@ class LogInViewController: UIViewController {
             .foregroundColor: UIColor.black,
             .font: UIFont.boldSystemFont(ofSize: 16.0)
         ])
-        
     }
     
-
-    @IBAction func loginTapped(_ sender: Any) {
+    @IBAction private func loginTapped(_ sender: Any) {
         let userEmail = emailTextField.text
         let userPassword = passwordTextField.text
         
-        if(userEmail == "" || userPassword == "") {
+        if userEmail == "" || userPassword == "" {
             alertMessage(userMessage: "All fields are required")
         } else {
-            let mail = isValidEmail(emailID: userEmail!)
-            if(mail == false) {
+            let mail = isValidEmail(emailID: userEmail)
+            if mail == false {
                 alertMessage(userMessage: "Please enter a valid email addres!")
                 emailLabel.textColor = .red
             } else {
-                if(isValidPassword(password: userPassword!) == false ) {
+                if isValidPassword(password: userPassword) == false {
                     alertMessage(userMessage: "Please enter a valid password!")
-                    emailLabel.textColor = colorText
+                    emailLabel.textColor = .colorText
                     passwordLabel.textColor = .red
                 } else {
                     alertMessage(userMessage: "Success!")
-                    passwordLabel.textColor = colorText
+                    passwordLabel.textColor = .colorText
                 }
             }
         }
     }
     
-    
     @IBAction func signupTapped(_ sender: Any) {
     }
 
     //Email validation
-    func isValidEmail(emailID: String) ->Bool {
+    private func isValidEmail(emailID: String?) ->Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: emailID)
     }
     
     //Password validation
-    func isValidPassword(password: String) ->Bool{
+    private func isValidPassword(password: String?) ->Bool{
         let passwordReg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$"
         let passwordTesting = NSPredicate(format: "SELF MATCHES %@", passwordReg)
         return passwordTesting.evaluate(with: password)
     }
     
     //Alert
-    func alertMessage(userMessage: String) {
+    private func alertMessage(userMessage: String) {
         let myAllert = UIAlertController(title: "Something went wrong", message: userMessage, preferredStyle: UIAlertController.Style.alert)
         let ok = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:  nil)
         myAllert.addAction(ok)
         present(myAllert, animated: true, completion: nil)
     }
    
-class Utilities {
-    
+    class Utilities {
     //Style the text fields
-    static func styleTextField(textfield:UITextField) {
-        
+    final class func styleTextField(textfield:UITextField) {
         // Create the bottom line
         let bottomLine = CALayer()
         bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 1)
@@ -149,14 +139,14 @@ class Utilities {
     }
     
     //Style the Log in Button
-    static func styleLogInButton(button: UIButton) {
+    final class func styleLogInButton(button: UIButton) {
         button.backgroundColor = UIColor.white
         button.layer.cornerRadius = 15
         button.tintColor = UIColor.black
     }
     
     // Adding shadow to the button
-    static func styleShadowButton(button: UIButton){
+    final class func styleShadowButton(button: UIButton) {
         button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
         button.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
         button.layer.shadowOpacity = 1.0
@@ -164,6 +154,10 @@ class Utilities {
         button.layer.masksToBounds = false
     }
 }
-    
+}
+
+extension UIColor{
+   static let colorText = UIColor(red: 37/255, green: 22/255, blue: 5/255, alpha: 1)
+   static let colorView = UIColor(red: 124/255, green: 122/255, blue: 122/255, alpha: 1)
 }
 
