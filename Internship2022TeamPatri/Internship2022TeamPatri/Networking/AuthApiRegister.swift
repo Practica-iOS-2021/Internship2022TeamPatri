81b715242e2a217dab3359e91842bc78b021b0f6
Eduard Caragea <eduardcaragea@gmail.com>
2022-08-03 15:54:46 +0300

    Implemented Register API

---
+++ Internship2022TeamPatri/Internship2022TeamPatri/Networking/AuthApiRegister.swift
@@ -0,0 +1,40 @@
+//
+//  AuthApiRegister.swift
+//  Internship2022TeamPatri
+//
+//  Created by Eduard Sorin Caragea on 03.08.2022.
+//
+
+import Foundation
+import FirebaseAuth
+import Firebase
+
+class AuthApiRegister {
+    
+    static let sharedInstance = AuthApiRegister()
+    func register(user: User, completion: @escaping (Bool) -> Void) {
+        
+        guard let email = user.email?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let name = user.name?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let personalID = user.personalID?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let studentID = user.studentID?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let password = user.password?.trimmingCharacters(in: .whitespacesAndNewlines)
+        else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if error != nil {
+                completion(false)
+            } else { // User was created, now store the dates
+                let db = Firestore.firestore()
+                guard let uid = result?.user.uid else { return }
+                db.collection("users").document(uid).setData(["email":email, "name": name, "personalID": personalID, "studentID": studentID]) { (error) in
+                    if error != nil{
+                        completion(false)
+                    }
+                }
+            }
+            completion(true)
+        }
+    }
+}
+
