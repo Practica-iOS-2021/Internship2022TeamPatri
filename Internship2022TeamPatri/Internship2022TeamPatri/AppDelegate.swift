108071362f54e1753b8f6e930e19a903c7015763
Muresan Coralia <coralia.muresan99@gmail.com>
2022-08-03 11:07:55 +0300

    fetch data func

--- Internship2022TeamPatri/Internship2022TeamPatri/AppDelegate.swift
+++ Internship2022TeamPatri/Internship2022TeamPatri/AppDelegate.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    //let db = Firestore.firestore()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- Internship2022TeamPatri/Internship2022TeamPatri/Models/RetrieveUserData.swift
+++ Internship2022TeamPatri/Internship2022TeamPatri/Models/RetrieveUserData.swift
@@ -6,8 +6,37 @@
 //
 
 import Foundation
+import FirebaseFirestore
 import UIKit
+import simd
 
-class RetrieveUserData {
+class RetrieveUserData: ObservableObject {
     
+    @Published var user = [Users]()
+    private var db = Firestore.firestore()
+    
+    func fetchData() {
+        db.collection("user").addSnapshotListener {(QuerySnapshot, error) in
+            guard let documents = QuerySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            self.user = documents.map {(QueryDocumentSnapshot) -> Users in
+                let data = QueryDocumentSnapshot.data()
+
+                let name = data["Name"] as? String ?? ""
+                let email = data["Email"] as? String ?? ""
+                let personalID = data["PersonalID"] as? String ?? ""
+                let studentID = data["StudentID"] as? String ?? ""
+                let photo = data[""] as? UIImage ?? nil
+                let chapter = data["Chapter"] as? String ?? ""
+                let courses = data["Courses"] as? String ?? ""
+                let grade = data["Grade"] as? Int ?? 0
+                let semester = data["Semester"] as? Int ?? 0
+                
+                return Users(name: name, email: email, personalID: personalID, studentID: studentID, photo: photo, grades: Users.Grades(chapter: chapter, courses: courses, grade: grade, semester: semester))
+            }
+        }
+    }
 }
