fbfa61961d39c56b0174471b3383da20f0f0a788
MuresanCoralia <83501334+MuresanCoralia@users.noreply.github.com>
2022-08-09 09:16:09 +0300

    Register Screen (#12)
    * moving folder, adding regex for personalID and other small changes
    
    * changing from the conflicts
    
    * changing indentation
    
    * optimizing color label
    
    * color label changes
    
    * changing views names
    
    * adding the custom back button
    
    * very small change
    
    * small ui changes

--- Internship2022TeamPatri/Internship2022TeamPatri/Controllers/Auth/Login/LogInViewController.swift
+++ Internship2022TeamPatri/Internship2022TeamPatri/Controllers/Auth/Login/LogInViewController.swift
@@ -85,6 +85,7 @@ class LogInViewController: UIViewController {
             if mail == false {
                 alertMessage(title: "Something went wrong!", userMessage: "Please enter a valid email addres!")
                 emailLabel.textColor = .red
+                passwordLabel.textColor = .colorText
             } else {
                 if isValidPassword(password: userPassword) == false {
                     alertMessage(title: "Something went wrong!", userMessage: "Please enter a valid password!")
@@ -93,14 +94,22 @@ class LogInViewController: UIViewController {
                 } else {
                     alertMessage(title: "You are successfully logged in", userMessage: "Success!")
                     passwordLabel.textColor = .colorText
+                    emailLabel.textColor = .colorText
                 }
             }
         }
     }
     
+    // sign up button (going to register screen)
     @IBAction private func signupTapped(_ sender: Any) {
+        // moving through a navigation controller and having a customed back button
         let registerViewController = RegisterViewController.loadFromNib()
         self.navigationController?.pushViewController(registerViewController, animated: true)
+        let backButtonArrow = UIImage(named: "Vector")
+        self.navigationController?.navigationBar.backIndicatorImage = backButtonArrow
+        self.navigationController?.navigationBar.tintColor = UIColor.colorText
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = backButtonArrow
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItem.Style.plain, target: nil, action: nil)
     }
     
     //Email validation
