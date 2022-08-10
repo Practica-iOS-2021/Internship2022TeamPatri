6f3e543944134e2c2c7366b0b83ee95d3079ccf6
Vlad Silviu Hagiu <vladsilviu.hagiu@endava.com>
2022-08-10 18:56:44 +0300

    Resolved code review

--- Internship2022TeamPatri/Internship2022TeamPatri/Controllers/Home/HomeTabBarController.swift
+++ Internship2022TeamPatri/Internship2022TeamPatri/Controllers/Home/HomeTabBarController.swift
@@ -18,28 +18,30 @@ class HomeTabBarController: UITabBarController {
     
     private func setViewController() {
         let storyboard = UIStoryboard(name: "GradesView", bundle: nil)
+        
         let gradesViewController = storyboard.instantiateViewController(withIdentifier: "GradesViewController")
+        let gradesNavController = createNavigationController(viewController: gradesViewController, imageName: "grades")
         
         let coursesViewController = CoursesViewController.loadFromNib()
-        let navController = UINavigationController(rootViewController: coursesViewController)
-        let profileViewController = ProfileViewController()
+        let coursesNavController = createNavigationController(viewController: coursesViewController, imageName: "courses")
         
-        self.setViewControllers([ gradesViewController,navController, profileViewController], animated: false)
+        let profileViewController = ProfileViewController()
+        let profileNavController = createNavigationController(viewController: profileViewController, imageName: "profile")
+        
+        self.setViewControllers([gradesNavController, coursesNavController, profileNavController], animated: false)
     }
     
-    func setTabBarItems() {
-        setItem(imageName: "grades", tabBarItem: tabBar.items?[0])
-        setItem(imageName: "courses", tabBarItem: tabBar.items?[1])
-        setItem(imageName: "profile", tabBarItem: tabBar.items?[2])
+    private func createNavigationController(viewController: UIViewController, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.image = UIImage(named: imageName)?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
+        return navController
     }
     
-    private func setItem(imageName: String, tabBarItem: UITabBarItem?){
-        guard let tabBarItem = tabBarItem else {
-            return
+    private func setTabBarItems() {
+        tabBar.items?.forEach {
+            $0.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+            $0.selectedImage = UIImage(named: "Selected ")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         }
-        tabBarItem.image = UIImage(named: imageName)?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
-        tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-        tabBarItem.selectedImage = UIImage(named: "Selected ")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
     }
     
 }
