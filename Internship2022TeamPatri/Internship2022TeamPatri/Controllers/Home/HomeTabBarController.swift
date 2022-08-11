4bf1c9e99b64b2637afaa3f22fb1eeceb74ee0de
HVlad1 <98744385+HVlad1@users.noreply.github.com>
2022-08-11 09:10:36 +0300

    Changed tabBar from storyboard to programmatically (#17)
    * Changed tabBar from storyboard to programmatically
    
    * Code review
    
    * Resolved code review
    
    Co-authored-by: Vlad Silviu Hagiu <vladsilviu.hagiu@endava.com>

--- Internship2022TeamPatri/Internship2022TeamPatri/Controllers/Home/HomeTabBarController.swift
+++ Internship2022TeamPatri/Internship2022TeamPatri/Controllers/Home/HomeTabBarController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class HomeTabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setViewController()
+        setTabBarItems()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setViewController() {
+        let storyboard = UIStoryboard(name: "GradesView", bundle: nil)
+        
+        let gradesViewController = storyboard.instantiateViewController(withIdentifier: "GradesViewController")
+        let gradesNavController = createNavigationController(viewController: gradesViewController, imageName: "grades")
+        
+        let coursesViewController = CoursesViewController.loadFromNib()
+        let coursesNavController = createNavigationController(viewController: coursesViewController, imageName: "courses")
+        
+        let profileViewController = ProfileViewController()
+        let profileNavController = createNavigationController(viewController: profileViewController, imageName: "profile")
+        
+        self.setViewControllers([gradesNavController, coursesNavController, profileNavController], animated: false)
     }
-    */
-
+    
+    private func createNavigationController(viewController: UIViewController, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.image = UIImage(named: imageName)?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
+        return navController
+    }
+    
+    private func setTabBarItems() {
+        tabBar.items?.forEach {
+            $0.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+            $0.selectedImage = UIImage(named: "Selected ")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
+        }
+    }
+    
 }
