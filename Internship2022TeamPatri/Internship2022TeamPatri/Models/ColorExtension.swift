73443d2251c0505a9717335a532a23bd6718e263
MuresanCoralia <83501334+MuresanCoralia@users.noreply.github.com>
2022-08-12 09:00:49 +0300

    Feature/user profile (#16)
    * Merge branch 'develop' into profileScreen
    
    # Conflicts:
    #	Internship2022TeamPatri/Internship2022TeamPatri.xcodeproj/project.pbxproj
    #	Internship2022TeamPatri/Internship2022TeamPatri/Views/Auth/Login/LogIn.storyboard
    
    * solving issues
    
    * fix indentation
    
    * fixed indentations
    
    * log out function
    
    * fixing space issue
    
    * function to display personal info on screen
    
    * adding the image
    
    * log out function and button
    
    * small change
    
    * function log out
    
    * delete extra lines and spaces
    
    * delete log out function

--- Internship2022TeamPatri/Internship2022TeamPatri/Models/ColorExtension.swift
+++ Internship2022TeamPatri/Internship2022TeamPatri/Models/ColorExtension.swift
@@ -12,4 +12,6 @@ import UIKit
      static let colorView = UIColor(red: 0.486, green: 0.478, blue: 0.478, alpha: 1)
      static let colorText = UIColor(red: 0.145, green: 0.0862, blue: 0.0196, alpha: 1)
      static let colorBackground = UIColor(red: 0.752, green: 0.937, blue: 0.952, alpha: 1)
+     static let colorLabelProfile = UIColor(red: 0.164, green: 0.097, blue: 0.022, alpha: 1)
+     static let colorTextProfile = UIColor(red: 0.551, green: 0.542, blue: 0.542, alpha: 1)
 }
